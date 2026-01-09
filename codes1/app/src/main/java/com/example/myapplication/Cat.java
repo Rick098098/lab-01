a8aae40aa38a8fc8b804e1d33208213eb4c4fe5c
Yixing Li <yixingli@YixingdeMacBook-Air.local>
2026-01-08 18:07:19 -0700

    first try

---
+++ codes1/app/src/main/java/com/example/myapplication/Cat.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import java.util.Date;
+public class Cat extends Pet implements Pettable{
+
+    public Cat(String name){
+        super(name);
+    }
+
+    public Cat(String name, Date birthDate){
+        super(name, birthDate);
+    }
+    public String speak(){
+        return"meotherfucker!";
+    }
+    public void pet(){
+
+    }
+}
---
+++ codes1/app/src/main/java/com/example/myapplication/Dog.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import java.util.Date;
+public class Dog extends Pet implements Pettable {
+
+    public Dog(String name){
+        super(name);
+    }
+
+    public Dog(String name, Date birthDate){
+        super(name, birthDate);
+    }
+    public String speak(){
+        return"meotherfucker!";
+    }
+
+    public void pet(){
+
+    }
+}
